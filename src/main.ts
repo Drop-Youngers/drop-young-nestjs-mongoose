72c941ce0f6d6a724acadcf159e8bbbbd644cccf
Best Verie Iradukunda <60257249+Best-Verie@users.noreply.github.com>
2021-08-01 18:25:52 +0200

    added swagger ui

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Notepen Api')
+    .setDescription('Notepen backend Apis')
+    .setVersion('1.0')
+    .addTag('users')
+    .addTag('user-roles')
+    .addTag('books')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
