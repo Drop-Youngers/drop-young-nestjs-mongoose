f639c8bfafc4cd45bae831101d19eb807ba6a9a2
Best Verie Iradukunda <60257249+Best-Verie@users.noreply.github.com>
2021-08-02 12:01:59 +0200

    signUpwithEmail

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,13 +1,46 @@
-import { Injectable } from '@nestjs/common';
+import { AccountType } from './../utils/enums/accountTypes.enum';
+import { Injectable, NotAcceptableException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { User } from './user.model';
+const { hashPassword} = require('../utils/hashes/password.hash');
 
 @Injectable()
 export class UserService {
     constructor(@InjectModel('User') private readonly userModel:Model<User>,) {}
 
     async createUser(data:any, accType: AccountType){
+        let {firstname,lastname,email, gender, password,accountType} = data;
 
+        const userExist = await this.findUserByEmail(email);
+        if(userExist.found && userExist.user.accountStatus==1){
+            throw new NotAcceptableException('User already exist');
+        }
+
+        if(accType==AccountType.email){
+            return this.createUserWithEmail(data);
+        }
+        else if(accType==AccountType.google){
+            return this.createUserWithGoogle(data);
+        }
     }
+    async findUserByEmail(email: string){
+        const user = await this.userModel.findOne({email: email});
+        if(user)
+        return {
+            found: true,
+            user
+        }
+
+        return{
+            found: false
+        }
+    }
+    async createUserWithEmail(data){
+        let {firstname,lastname,email,gender,password,userType} = data;  
+        const accountType = AccountType.email;
+        password = await hashPassword(password); 
+        const newUser = await this.userModel({email,firstname,})
+    }
+    
 }
