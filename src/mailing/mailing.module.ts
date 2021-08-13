74a341d1378017854af0dd1b1a4e49fdf65abf94
Best Verie Iradukunda <60257249+Best-Verie@users.noreply.github.com>
2021-08-13 23:10:55 +0200

    account verification prt1

--- src/mailing/mailing.module.ts
+++ src/mailing/mailing.module.ts
@@ -1,4 +1,51 @@
-import { Module } from '@nestjs/common';
 
-@Module({})
+import { ConfigModule } from '@nestjs/config';
+import { JwtService, JwtModule } from '@nestjs/jwt';
+import { Module } from '@nestjs/common';
+import { MailerModule } from '@nestjs-modules/mailer';
+import {HandlebarsAdapter} from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
+import { MailingService } from './mailing.service';
+import {MongooseModule} from '@nestjs/mongoose';
+import {verifyAccountSchema} from '../user/verifyAccount.model';
+import {UserSchema} from '../user/user.model';
+import {AuthService} from '../auth/auth.service';
+import configurations from '../config/configurations';
+import {SharedModule} from '../shared/shared.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({envFilePath: `src/config/${process.env.NODE_ENV}.env`, load: [configurations]}),
+    MongooseModule.forFeature([
+    {name: 'VerifyAccount', schema: verifyAccountSchema},
+    {name: 'User', schema: UserSchema}
+  ]),
+  JwtModule.register({
+    secret: process.env.SECRET_KEY,
+  }),
+  SharedModule,
+    MailerModule.forRoot({
+    transport: {
+      host: process.env.EMAIL_HOST,
+      port: process.env.EMAIL_PORT,
+      secure: true,
+      auth:{
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS
+      }
+    },
+    defaults: {
+      from: `"nest-modules" ${process.env.EMAIL_USER}`,
+    },
+    template: {
+      dir: 'src/templates/',
+      adapter: new HandlebarsAdapter(),
+      options: {
+        strict: true
+      }
+    }
+  }),
+ 
+  ],
+  providers: [MailingService, AuthService]
+})
 export class MailingModule {}
--- src/shared/shared.module.ts
+++ src/shared/shared.module.ts
@@ -2,18 +2,23 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import {JwtModule } from '@nestjs/jwt';
 import { UserSchema } from './../user/user.model';
+import {verifyAccountSchema} from '../user/verifyAccount.model';
 
 @Module({
     imports: [
         MongooseModule.forFeature([
           {name: 'User', schema: UserSchema},
+          {name: 'VerifyAccount', schema: verifyAccountSchema},
+
       ]),
         JwtModule.register({
           secret: process.env.SECRET_KEY,
         }),
      ],
      exports: [
-        MongooseModule.forFeature([{name: 'User', schema: UserSchema}]),
+        MongooseModule.forFeature([{name: 'User', schema: UserSchema},
+        {name: 'VerifyAccount', schema: verifyAccountSchema},
+    ]),
         JwtModule.register({
           secret: process.env.SECRET_KEY,
         }),
